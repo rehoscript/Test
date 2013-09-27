5960f7118cfa5a12adc5e14930f3957d64c62e35
Hector Morales Palma <rehoscript@gmail.com>
2013-09-26 23:46:56 -0500

    okas

--- inc/Config.server.php
+++ inc/Config.server.php
@@ -1,17 +1,17 @@
 <?php
 //   Date             Modified by         Change(s)
 //   2013-09-24         HMP                 1.0
-	define("ERRORTRACE",FALSE);
-	error_reporting(0);
-	define("DS",DIRECTORY_SEPARATOR);
-	define("ABSPATH", "http://qrfastfood.teracom.mx/".DS);
-	define("DB_NAME","postgres");
-	define("USER","postgres");
-	define("PASS","maped1819");
-	define("HOST","localhost");
-	define("ANALITYCS",FALSE);
-        define("RESULTTRACE",FALSE);
-        define("MXDBG",FALSE);
+define("ERRORTRACE",FALSE);
+error_reporting(0);
+define("DS",DIRECTORY_SEPARATOR);
+define("ABSPATH", "http://qrfastfood.teracom.mx/".DS);
+define("DB_NAME","postgres");
+define("USER","postgres");
+define("PASS","maped1819");
+define("HOST","localhost");
+define("ANALITYCS",FALSE);
+define("RESULTTRACE",FALSE);
+define("MXDBG",FALSE);
 
-		echo 'entro server';
+	
 ?>
