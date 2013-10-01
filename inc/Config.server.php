6501f29b18a393f3c81495e9a7178f783e668e60
Hector Morales Palma <rehoscript@gmail.com>
2013-10-01 16:42:08 -0500

    sda

--- inc/Config.server.php
+++ inc/Config.server.php
@@ -6,7 +6,7 @@ error_reporting(0);
 define("DS",DIRECTORY_SEPARATOR);
 define("ABSPATH", "http://www.wwww.wwww/".DS);
 define("DB_NAME","postgres");
-define("USER","postgres");
+define("USER","postgreqs");
 define("PASS","maped1819");
 define("HOST","localhost");
 define("PORT","5432");
