33150a7cd0f9f3bcc7140c26670726d704173038
Hector Morales Palma <rehoscript@gmail.com>
2013-10-01 16:42:58 -0500

    Porter

--- inc/Config.server.php
+++ inc/Config.server.php
@@ -5,14 +5,14 @@ define("ERRORTRACE",FALSE);
 error_reporting(0);
 define("DS",DIRECTORY_SEPARATOR);
 define("ABSPATH", "http://www.wwww.wwww/".DS);
-define("DB_NAME","postgres");
-define("USER","postgreqs");
-define("PASS","maped1819");
-define("HOST","localhost");
+define("DB_NAME","softmeta");
+define("USER","softmeta");
+define("PASS","s0ftm3t6-.");
+define("HOST","192.168.57.41");
 define("PORT","5432");
 define("ANALITYCS",FALSE);
 define("RESULTTRACE",FALSE);
 define("MXDBG",FALSE);
 
-	
+
 ?>
--- inc/howtouse.php
+++ inc/howtouse.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'Query.php';
 
+
 $sql=new Query();
 //$sql->insert("test2",
 //             "campo",
@@ -10,19 +11,20 @@ $sql=new Query();
 //$sql->update("UPDATE test2 SET campo=1111 where campo=1");
 
 
-$sql->delete("test2","campo = 6");
+//$sql->delete("test2","campo = 6");
 
 
-//$sql->sql="SELECT campo from test2";
-////$resultado= $sql->select("obj");
-////
-////
-////foreach ($resultado as $res)
-////{
-////    echo $res->campo;
-////    echo "<br/>";    
-////}
-////echo "----------------------";
+//$sql->sql="SELECT * from pg_am";
+//$resultado= $sql->select("obj");
+//
+//echo $resultado;
+
+//foreach ($resultado as $res)
+//{
+//    echo $res->campo;
+//    echo "<br/>";    
+//}
+//echo "----------------------";
 ////echo "<br/>";
 ////
 //
