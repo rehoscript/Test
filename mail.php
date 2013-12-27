75e47571dc1ef5c6645660f9ecdd43405f3601e9
Héctor Morales Palma <rehoscript@gmail.com>
2013-12-26 22:54:16 -0600

    test

--- mail.php
+++ mail.php
@@ -1,23 +1,11 @@
 <?php
-require_once 'Swift-5.0.3/lib/swift_required.php';
-
-$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com',
-                                              465,
-                                              'ssl')
-             ->setUsername('hectormoralespalma@gmail.com')
-             ->setPassword('maped1819');
- 
-//Creamos el mailer pasándole el transport con la configuración de gmail
-$mailer = Swift_Mailer::newInstance($transport);
- 
-//Creamos el mensaje
-$message = Swift_Message::newInstance($subject)
-            ->setFrom(array('rehoscript@gmail.com' => 'Blackslot'))
-            ->setTo("rehoscript@gmail.com")
-            ->setBody("hola");
- 
-//Enviamos
-echo $result = $mailer->send($message);
+$headers .= "Organization: Sender Organization\r\n";
+  $headers .= "MIME-Version: 1.0\r\n";
+  $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+  $headers .= "X-Priority: 3\r\n";
+  $headers .= "X-Mailer: PHP". phpversion() ."\r\n" ;
     
+mail("rehoscript@gmail.com", "Message", "A simple message.", $headers); 
+
 
 ?>
