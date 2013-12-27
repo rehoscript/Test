3dcd9d115b21afe549d39947389300b9608c58bf
Héctor Morales Palma <rehoscript@gmail.com>
2013-12-26 22:59:29 -0600

    test

--- mail.php
+++ mail.php
@@ -2,6 +2,8 @@
 $headers .= "Organization: Sender Organization\r\n";
   $headers .= "MIME-Version: 1.0\r\n";
   $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+  $headers .= "From: MetaSpace";
+  
   $headers .= "X-Priority: 3\r\n";
   $headers .= "X-Mailer: PHP". phpversion() ."\r\n" ;
     
