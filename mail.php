aeda4f8a1a4d585c44639a80cb29a3d19d792e1e
Héctor Morales Palma <rehoscript@gmail.com>
2013-12-26 23:03:35 -0600

    test

--- mail.php
+++ mail.php
@@ -6,6 +6,7 @@ $headers .= "Organization: Sender Organization\r\n";
   
   $headers .= "X-Priority: 3\r\n";
   $headers .= "X-Mailer: PHP". phpversion() ."\r\n" ;
+   $headers .= "Reply-To: hectormoralespalma@gmail.com\r\n";
    $headers .= "From: MetaSpace";
     
 mail("rehoscript@gmail.com", "Message", "A simple message.", $headers); 
