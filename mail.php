bfe897224581bb89a2e4b08df60b6eca1f6398e3
Héctor Morales Palma <rehoscript@gmail.com>
2013-12-28 13:58:55 -0600

    git

--- mail.php
+++ mail.php
@@ -74,7 +74,7 @@ $mail->Debugoutput = 'html';
 //Set the hostname of the mail server
 $mail->Host = "smtp.gmail.com";
 //Set the SMTP port number - likely to be 25, 465 or 587
-$mail->Port = 25;
+$mail->Port = 465;
 //Whether to use SMTP authentication
 $mail->SMTPAuth = true;
 //Username to use for SMTP authentication
