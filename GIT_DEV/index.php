5fa95895574b389aefcd6285be0d3af6262ae76b
Héctor Morales Palma <rehoscript@gmail.com>
2014-01-04 22:08:56 -0600

    git

---
+++ GIT_DEV/index.php
@@ -0,0 +1,362 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Dashboard - SB Admin</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Add custom CSS here -->
+    <link href="css/sb-admin.css" rel="stylesheet">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <!-- Page Specific CSS -->
+    <link rel="stylesheet" href="http://cdn.oesmith.co.uk/morris-0.4.3.min.css">
+  </head>
+
+  <body>
+
+    <div id="wrapper">
+
+      <!-- Sidebar -->
+      <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.html">SB Admin</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+
+        </div><!-- /.navbar-collapse -->
+      </nav>
+
+      
+
+    </div><!-- /#wrapper -->
+
+    
+    <div id="page-wrapper">
+
+        
+
+        <div class="row">
+          <div class="col-lg-3">
+            <div class="panel panel-info">
+              <div class="panel-heading">
+                <div class="row">
+                  <div class="col-xs-6">
+                    <i class="fa fa-comments fa-5x"></i>
+                  </div>
+                  <div class="col-xs-6 text-right">
+                    <p class="announcement-heading">456</p>
+                    <p class="announcement-text">New Mentions!</p>
+                  </div>
+                </div>
+              </div>
+              <a href="#">
+                <div class="panel-footer announcement-bottom">
+                  <div class="row">
+                    <div class="col-xs-6">
+                      View Mentions
+                    </div>
+                    <div class="col-xs-6 text-right">
+                      <i class="fa fa-arrow-circle-right"></i>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="col-lg-3">
+            <div class="panel panel-warning">
+              <div class="panel-heading">
+                <div class="row">
+                  <div class="col-xs-6">
+                    <i class="fa fa-check fa-5x"></i>
+                  </div>
+                  <div class="col-xs-6 text-right">
+                    <p class="announcement-heading">12</p>
+                    <p class="announcement-text">To-Do Items</p>
+                  </div>
+                </div>
+              </div>
+              <a href="#">
+                <div class="panel-footer announcement-bottom">
+                  <div class="row">
+                    <div class="col-xs-6">
+                      Complete Tasks
+                    </div>
+                    <div class="col-xs-6 text-right">
+                      <i class="fa fa-arrow-circle-right"></i>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="col-lg-3">
+            <div class="panel panel-danger">
+              <div class="panel-heading">
+                <div class="row">
+                  <div class="col-xs-6">
+                    <i class="fa fa-tasks fa-5x"></i>
+                  </div>
+                  <div class="col-xs-6 text-right">
+                    <p class="announcement-heading">18</p>
+                    <p class="announcement-text">Crawl Errors</p>
+                  </div>
+                </div>
+              </div>
+              <a href="#">
+                <div class="panel-footer announcement-bottom">
+                  <div class="row">
+                    <div class="col-xs-6">
+                      Fix Issues
+                    </div>
+                    <div class="col-xs-6 text-right">
+                      <i class="fa fa-arrow-circle-right"></i>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="col-lg-3">
+            <div class="panel panel-success">
+              <div class="panel-heading">
+                <div class="row">
+                  <div class="col-xs-6">
+                    <i class="fa fa-comments fa-5x"></i>
+                  </div>
+                  <div class="col-xs-6 text-right">
+                    <p class="announcement-heading">56</p>
+                    <p class="announcement-text">New Orders!</p>
+                  </div>
+                </div>
+              </div>
+              <a href="#">
+                <div class="panel-footer announcement-bottom">
+                  <div class="row">
+                    <div class="col-xs-6">
+                      Complete Orders
+                    </div>
+                    <div class="col-xs-6 text-right">
+                      <i class="fa fa-arrow-circle-right"></i>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div><!-- /.row -->
+
+       
+
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="panel panel-primary">
+              <div class="panel-heading">
+                <h3 class="panel-title"><i class="fa fa-clock-o"></i> Héctor</h3>
+              </div>
+              <div class="panel-body">
+                <div class="list-group">
+                  
+                  <a href="#" class="list-group-item">
+                    
+                    <i class="fa fa-globe"></i> HOLA 3
+                  </a>
+                   <a href="#" class="list-group-item">
+                    
+                    <i class="fa fa-calendar"></i> 12/12/12
+                  </a>
+                  <a href="#" class="list-group-item">
+                    
+                    <i class="fa fa-check"></i>Archivos:
+                    <br/>
+                    <ul>
+                        <li>
+                            a.js
+                        </li>
+                        <li>
+                            b.js
+                        </li>
+                    </ul>
+                  </a>
+                </div>
+                  <div class="list-group">
+                  
+                  <a href="#" class="list-group-item">
+                    
+                    <i class="fa fa-globe"></i> HOLA 3
+                  </a>
+                   <a href="#" class="list-group-item">
+                    
+                    <i class="fa fa-calendar"></i> 12/12/12
+                  </a>
+                  <a href="#" class="list-group-item">
+                    
+                    <i class="fa fa-check"></i>Archivos:
+                    <br/>
+                    <ul>
+                        <li>
+                            a.js
+                        </li>
+                        <li>
+                            b.js
+                        </li>
+                    </ul>
+                  </a>
+                </div>
+                <div class="text-right">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+            <div class="col-lg-4">
+            <div class="panel panel-primary">
+              <div class="panel-heading">
+                <h3 class="panel-title"><i class="fa fa-clock-o"></i> Omar</h3>
+              </div>
+              <div class="panel-body">
+                <div class="list-group">
+                  
+                  <a href="#" class="list-group-item">
+                    
+                    <i class="fa fa-globe"></i> HOLA 3
+                  </a>
+                   <a href="#" class="list-group-item">
+                    
+                    <i class="fa fa-calendar"></i> 12/12/12
+                  </a>
+                  <a href="#" class="list-group-item">
+                    
+                    <i class="fa fa-check"></i>Archivos:
+                    <br/>
+                    <ul>
+                        <li>
+                            a.js
+                        </li>
+                        <li>
+                            b.js
+                        </li>
+                    </ul>
+                  </a>
+                </div>
+                  <div class="list-group">
+                  
+                  <a href="#" class="list-group-item">
+                    
+                    <i class="fa fa-globe"></i> HOLA 3
+                  </a>
+                   <a href="#" class="list-group-item">
+                    
+                    <i class="fa fa-calendar"></i> 12/12/12
+                  </a>
+                  <a href="#" class="list-group-item">
+                    
+                    <i class="fa fa-check"></i>Archivos:
+                    <br/>
+                    <ul>
+                        <li>
+                            a.js
+                        </li>
+                        <li>
+                            b.js
+                        </li>
+                    </ul>
+                  </a>
+                </div>
+                <div class="text-right">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+            <div class="col-lg-4">
+            <div class="panel panel-primary">
+              <div class="panel-heading">
+                <h3 class="panel-title"><i class="fa fa-clock-o"></i> Manuel</h3>
+              </div>
+              <div class="panel-body">
+                <div class="list-group">
+                  
+                  <a href="#" class="list-group-item">
+                    
+                    <i class="fa fa-globe"></i> HOLA 3
+                  </a>
+                   <a href="#" class="list-group-item">
+                    
+                    <i class="fa fa-calendar"></i> 12/12/12
+                  </a>
+                  <a href="#" class="list-group-item">
+                    
+                    <i class="fa fa-check"></i>Archivos:
+                    <br/>
+                    <ul>
+                        <li>
+                            a.js
+                        </li>
+                        <li>
+                            b.js
+                        </li>
+                    </ul>
+                  </a>
+                </div>
+                  <div class="list-group">
+                  
+                  <a href="#" class="list-group-item">
+                    
+                    <i class="fa fa-globe"></i> HOLA 3
+                  </a>
+                   <a href="#" class="list-group-item">
+                    
+                    <i class="fa fa-calendar"></i> 12/12/12
+                  </a>
+                  <a href="#" class="list-group-item">
+                    
+                    <i class="fa fa-check"></i>Archivos:
+                    <br/>
+                    <ul>
+                        <li>
+                            a.js
+                        </li>
+                        <li>
+                            b.js
+                        </li>
+                    </ul>
+                  </a>
+                </div>
+                <div class="text-right">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div><!-- /.row -->
+
+      </div><!-- /#page-wrapper -->
+    <!-- JavaScript -->
+    <script src="js/jquery-1.10.2.js"></script>
+    <script src="js/bootstrap.js"></script>
+
+    <!-- Page Specific Plugins -->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+    <script src="http://cdn.oesmith.co.uk/morris-0.4.3.min.js"></script>
+    <script src="js/morris/chart-data-morris.js"></script>
+    <script src="js/tablesorter/jquery.tablesorter.js"></script>
+    <script src="js/tablesorter/tables.js"></script>
+
+  </body>
+</html>
