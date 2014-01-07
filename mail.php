d4f557c7d786122e0b4ee7953f2c4e350f02f436
Héctor Morales Palma <rehoscript@gmail.com>
2014-01-07 17:12:47 -0600

    TEST

--- mail.php
+++ mail.php
@@ -1,15 +1,15 @@
 <?php
-//$headers .= "Organization: Sender Organization\r\n";
-//  $headers .= "MIME-Version: 1.0\r\n";
-//  $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
-// 
-//  
-//  $headers .= "X-Priority: 3\r\n";
-//  $headers .= "X-Mailer: PHP". phpversion() ."\r\n" ;
-//   $headers .= "Reply-To: hectormoralespalma@gmail.com\r\n";
-//   $headers .= "From: MetaSpace";
-//    
-//mail("rehoscript2@hotmail.com", "Message", "A simple message.", $headers); 
+$headers .= "Organization: Sender Organization\r\n";
+  $headers .= "MIME-Version: 1.0\r\n";
+  $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+ 
+  
+  $headers .= "X-Priority: 3\r\n";
+  $headers .= "X-Mailer: PHP". phpversion() ."\r\n" ;
+   $headers .= "Reply-To: hectormoralespalma@gmail.com\r\n";
+   $headers .= "From: MetaSpace";
+    
+mail("enriquegprc@terra.com.mx", "Message", "A simple message.", $headers); 
 //require ("PHPMailer-master/class.phpmailer.php");
 ////require("class.phpmailer.php");
 //$mail = new PHPMailer();
