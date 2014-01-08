b5abd21731c040108b6166962f30a29cf996b936
Héctor Morales Palma <rehoscript@gmail.com>
2014-01-07 23:44:57 -0600

    test

---
+++ FUNC.php
@@ -0,0 +1,61 @@
+<?php
+
+date_default_timezone_set('Etc/UTC');
+require "PHPMailer-master/PHPMailerAutoload.php";
+
+class Correo  {
+
+    
+//Create a new PHPMailer instance
+public $mail;
+
+
+
+    public function __construct() {
+        
+        $this->mail = new PHPMailer();
+//Tell PHPMailer to use SMTP
+$this->mail->isSMTP();
+$this->mail->Debugoutput = 'html';
+$this->mail->Host = "smtp.gmail.com";
+$this->mail->Port =  587;
+$this->mail->SMTPAuth = true;
+$this->mail->Username = "rehoscript@gmail.com";
+$this->mail->Password = "crackdown2";
+$this->mail->isHTML(true);
+$this->mail->SMTPSecure = "tls";
+$this->mail->setFrom('rehoscript@gmail.com', 'First Last');
+//Set an alternative reply-to address
+//$mail->addReplyTo('replyto@example.com', 'First Last');
+//Set who the message is to be sent to
+//$mail->addAddress('enriquegprc@terra.com.mx', 'John Doe');
+$this->mail->addAddress('hectormoralespalma@gmail.com', 'John Doe');
+//Set the subject line
+$this->mail->Subject = 'ASUNTO';
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+
+//Replace the plain text body with one created manually
+
+$this->mail->Body ="<h1>aaa<h1>";
+//Attach an image file
+//$mail->addAttachment('images/phpmailer_mini.gif');
+
+//send the message, check for errors
+       
+    }
+
+    public function run() {
+            if (!$this->mail->send() ) {
+                echo "Mailer Error: " . $this->mail->ErrorInfo;
+            } else {
+                echo "Message sent!";
+            }
+    }
+
+}
+$a= new Correo();
+$a->run();
+
+
+
--- mail.php
+++ mail.php
@@ -1,15 +1,15 @@
 <?php
-$headers .= "Organization: Sender Organization\r\n";
-  $headers .= "MIME-Version: 1.0\r\n";
-  $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
- 
-  
-  $headers .= "X-Priority: 3\r\n";
-  $headers .= "X-Mailer: PHP". phpversion() ."\r\n" ;
-   $headers .= "Reply-To: hectormoralespalma@gmail.com\r\n";
-   $headers .= "From: MetaSpace";
-    
-mail("09280476@ittoluca.edu.mx", "Message", "A simple message.", $headers); 
+//$headers .= "Organization: Sender Organization\r\n";
+//  $headers .= "MIME-Version: 1.0\r\n";
+//  $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+// 
+//  
+//  $headers .= "X-Priority: 3\r\n";
+//  $headers .= "X-Mailer: PHP". phpversion() ."\r\n" ;
+//   $headers .= "Reply-To: hectormoralespalma@gmail.com\r\n";
+//   $headers .= "From: MetaSpace";
+//    
+//mail("09280476@ittoluca.edu.mx", "Message", "A simple message.", $headers); 
 //require ("PHPMailer-master/class.phpmailer.php");
 ////require("class.phpmailer.php");
 //$mail = new PHPMailer();
@@ -56,57 +56,8 @@ mail("09280476@ittoluca.edu.mx", "Message", "A simple message.", $headers);
 
 //SMTP needs accurate times, and the PHP time zone MUST be set
 //This should be done in your php.ini, but this is how to do it if you don't have access to that
-date_default_timezone_set('Etc/UTC');
 
-require "PHPMailer-master/PHPMailerAutoload.php";
 
-//Create a new PHPMailer instance
-$mail = new PHPMailer();
-//Tell PHPMailer to use SMTP
-$mail->isSMTP();
-//Enable SMTP debugging
-// 0 = off (for production use)
-// 1 = client messages
-// 2 = client and server messages
-$mail->SMTPDebug = 2;
-//Ask for HTML-friendly debug output
-$mail->Debugoutput = 'html';
-//Set the hostname of the mail server
-$mail->Host = "smtp.gmail.com";
-//Set the SMTP port number - likely to be 25, 465 or 587
-$mail->Port =  587;
-//Whether to use SMTP authentication
-$mail->SMTPAuth = true;
-//Username to use for SMTP authentication
-$mail->Username = "rehoscript@gmail.com";
-//Password to use for SMTP authentication
-$mail->Password = "crackdown2";
-
-$mail->SMTPSecure = "tls";
-//Set who the message is to be sent from
-//$mail->setFrom('hectormoralespalma@gmail.com', 'First Last');
-//Set an alternative reply-to address
-//$mail->addReplyTo('replyto@example.com', 'First Last');
-//Set who the message is to be sent to
-//$mail->addAddress('enriquegprc@terra.com.mx', 'John Doe');
-$mail->addAddress('09280476@ittoluca.edu.mx', 'John Doe');
-//Set the subject line
-$mail->Subject = 'PHPMailer SMTP test';
-//Read an HTML message body from an external file, convert referenced images to embedded,
-//convert HTML into a basic plain-text alternative body
-$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
-//Replace the plain text body with one created manually
-$mail->AltBody = 'This is a plain-text message body';
-$mail->Body ="aa"; 
-//Attach an image file
-//$mail->addAttachment('images/phpmailer_mini.gif');
-
-//send the message, check for errors
-if (!$mail->send()) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
-} else {
-    echo "Message sent!";
-}
 ?>
 </body>
 </html>
