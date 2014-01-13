881cf02c33602ea37d916dd8a5e69c7d9e1b8045
Héctor Morales Palma <rehoscript@gmail.com>
2014-01-12 19:21:52 -0600

    TEST

--- mail.php
+++ mail.php
@@ -9,8 +9,9 @@ $headers .= "Organization: Sender Organization\r\n";
   $headers .= "Reply-To: hectormoralespalma@gmail.com\r\n";
   //$headers .= "From: MetaSpace <user@[192.241.217.91]>\r\n";
     
-echo mail("09280476@ittoluca.edu.mx", "Message", "A simple message.", $headers,'-f user@[192.241.217.91]'); 
-echo mail("rehoscript@gmail.com", "Message", "A simple message.", $headers,'-f user@[192.241.217.91]'); 
+//echo mail("09280476@ittoluca.edu.mx", "Message", "A simple message.", $headers,'-f user@[192.241.217.91]'); 
+//echo mail("rehoscript@gmail.com", "Message", "A simple message.", $headers,'-f user@[192.241.217.91]'); 
+echo mail("enriquegprc@terra.com.mx", "Message", "A simple message.", $headers,'-f user@[192.241.217.91]'); 
 //echo mail("09280476@ittoluca.edu.mx", "Message", "A simple message.", $headers); 
 //echo mail("rehoscript@gmail.com", "Message", "A simple message.",$headers); 
 
