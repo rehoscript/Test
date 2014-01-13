bf2afbfe1d49377b25c72597f9cec10cdfce17ba
Héctor Morales Palma <rehoscript@gmail.com>
2014-01-12 20:38:42 -0600

    teset}

--- mail.php
+++ mail.php
@@ -13,10 +13,10 @@ $headers .= "Organization: Sender Organization\r\n";
 //echo mail("09280476@ittoluca.edu.mx", "Message", "A simple message.", $headers,'-f user@[192.241.217.91]'); 
 //echo mail("rehoscript@gmail.com", "Message", "A simple message.", $headers,'-f user@[192.241.217.91]'); 
 
-echo mail("enriquegprc@terra.com.mx", "Message", "A simple message.", $headers); 
-echo mail("09280476@ittoluca.edu.mx", "Message", "A simple message.", $headers); 
-echo mail("rehoscript@gmail.com", "Message", "A simple message.",$headers); 
-echo mail("rehoscript2@hotmail.com", "Message", "A simple message.",$headers); 
+echo mail("enriquegprc@terra.com.mx", "Message", "A asimple message.".  uniqid(), $headers); 
+echo mail("09280476@ittoluca.edu.mx", "Message", "A simple message.".  uniqid(), $headers); 
+echo mail("rehoscript@gmail.com", "Message", "A simple message.".  uniqid(),$headers); 
+echo mail("rehoscript2@hotmail.com", "Message", "A simple message.".  uniqid(),$headers); 
 
 //require ("PHPMailer-master/class.phpmailer.php");
 ////require("class.phpmailer.php");
