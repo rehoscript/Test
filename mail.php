099d40313052e4ad9d559cb20328c28cb1d065ba
Héctor Morales Palma <rehoscript@gmail.com>
2014-01-12 20:01:00 -0600

    test

--- mail.php
+++ mail.php
@@ -11,9 +11,11 @@ $headers .= "Organization: Sender Organization\r\n";
     
 //echo mail("09280476@ittoluca.edu.mx", "Message", "A simple message.", $headers,'-f user@[192.241.217.91]'); 
 //echo mail("rehoscript@gmail.com", "Message", "A simple message.", $headers,'-f user@[192.241.217.91]'); 
+ini_set('sendmail_from', 'me@domain.com');
 echo mail("enriquegprc@terra.com.mx", "Message", "A simple message.", $headers); 
 echo mail("09280476@ittoluca.edu.mx", "Message", "A simple message.", $headers); 
 echo mail("rehoscript@gmail.com", "Message", "A simple message.",$headers); 
+echo mail("rehoscript2@hotmail.com", "Message", "A simple message.",$headers); 
 
 //require ("PHPMailer-master/class.phpmailer.php");
 ////require("class.phpmailer.php");
